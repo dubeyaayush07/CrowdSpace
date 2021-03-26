cb5a190a9e91266d5e34bf9646bf88aeb862e2a0
Ayush Dubey <dubeyaayush07@gmail.com>
2021-03-26 16:05:16 +0530

    second

--- app/src/main/java/com/crowdspace/crowdspace/ui/QueueFragment.kt
+++ app/src/main/java/com/crowdspace/crowdspace/ui/QueueFragment.kt
@@ -1,6 +1,7 @@
 package com.crowdspace.crowdspace.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import com.crowdspace.crowdspace.R
 import com.crowdspace.crowdspace.databinding.FragmentQueueBinding
 import com.crowdspace.crowdspace.model.Business
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -17,7 +20,7 @@ import com.google.firebase.ktx.Firebase
 class QueueFragment : Fragment() {
 
     private lateinit var binding: FragmentQueueBinding
-    private lateinit var business: Business
+    private  var business: Business? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,8 +34,36 @@ class QueueFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         business = QueueFragmentArgs.fromBundle(requireArguments()).selectedBusiness
-//        val businessDocument = Firebase.firestore.collection("businesses")
-//            .document(business.id.toString()).update()
+        val user = FirebaseAuth.getInstance().currentUser
+        check(user?.uid.toString())
+
+        binding.addBtn.setOnClickListener { view ->
+            val doc = Firebase.firestore.collection("businesses")
+                    .document(business!!.id.toString())
+            doc.update("queue", FieldValue.arrayUnion(user?.uid)).addOnSuccessListener {
+                doc.get().addOnSuccessListener {
+                    business = it.toObject(Business::class.java)
+                    check(user?.uid.toString())
+                }
+            }
+        }
+
+
+
+    }
+
+    private fun check(id: String) {
+        val index = business!!.queue!!.indexOfFirst {
+            it == id
+        }
+
+        if (index == -1) {
+            binding.addBtn.visibility = View.VISIBLE
+
+        } else {
+            binding.addBtn.visibility = View.GONE
+            binding.index.text = (index + 1).toString()
+        }
     }
 
 
