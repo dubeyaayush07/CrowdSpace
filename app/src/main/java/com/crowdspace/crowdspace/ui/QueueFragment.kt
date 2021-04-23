473afba5fbd60bdbfb3dff26cecbb8e8048200a1
Ayush Dubey <dubeyaayush07@gmail.com>
2021-04-23 16:50:17 +0530

    second

--- app/src/main/java/com/crowdspace/crowdspace/ui/QueueFragment.kt
+++ app/src/main/java/com/crowdspace/crowdspace/ui/QueueFragment.kt
@@ -9,12 +9,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.crowdspace.crowdspace.CaptureActivity
 import com.crowdspace.crowdspace.R
 import com.crowdspace.crowdspace.databinding.FragmentQueueBinding
 import com.crowdspace.crowdspace.model.Business
+import com.crowdspace.crowdspace.model.Form
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
@@ -22,6 +24,7 @@ import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.zxing.integration.android.IntentIntegrator
+import java.util.*
 
 
 class QueueFragment : Fragment() {
@@ -29,7 +32,7 @@ class QueueFragment : Fragment() {
     private lateinit var binding: FragmentQueueBinding
     private var business: Business? = null
     private lateinit var collection: CollectionReference
-    private var userId: String = ""
+    private var formId: String = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -44,18 +47,22 @@ class QueueFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        userId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val id = FirebaseAuth.getInstance().currentUser?.uid
+        Firebase.firestore.collection("forms")
+                .whereEqualTo("uid", id)
+                .whereEqualTo("bid", business?.id)
+                .get().addOnSuccessListener {
+                    val objects = it.toObjects(Form::class.java)
+                    if (objects.size > 0) {
+                        formId = objects[0].id.toString()
+                    }
+                    checkIndex(formId)
+                }
         setup()
-        checkIndex(userId)
+
 
         binding.addBtn.setOnClickListener { view ->
-            val doc = collection.document(business!!.id.toString())
-            doc.update("queue", FieldValue.arrayUnion(userId)).addOnSuccessListener {
-                doc.get().addOnSuccessListener {
-                    business = it.toObject(Business::class.java)
-                    checkIndex(userId)
-                }
-            }
+            findNavController().navigate(QueueFragmentDirections.actionQueueFragmentToFormFragment(business!!))
         }
 
         binding.scanBtn.setOnClickListener {
@@ -106,7 +113,7 @@ class QueueFragment : Fragment() {
             doc.update("status", "occupied").addOnSuccessListener {
                 doc.get().addOnSuccessListener {
                     business = it.toObject(Business::class.java)
-                    checkIndex(userId)
+                    checkIndex(formId)
                 }
             }
         }
